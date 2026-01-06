3bca5c85a2505bd28d26be7d4e0fa99a01f389ab
Asus <Asus@DESKTOP-SMKJHFK>
2026-01-06 21:26:36 +0200

    Add project files.

---
+++ CIFRUL LUI CEZAR/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIFRUL_LUI_CEZAR
+{
+    internal class Program
+    {
+
+            static string Cezar(string text, int cheie)
+            {
+                char[] rezultat = text.ToCharArray();
+
+                for (int i = 0; i < rezultat.Length; i++)
+                {
+                    if (rezultat[i] >= 'a' && rezultat[i] <= 'z')
+                    {
+                        rezultat[i] = (char)((rezultat[i] - 'a' + cheie + 26) % 26 + 'a');
+                    }
+                    else if (rezultat[i] >= 'A' && rezultat[i] <= 'Z')
+                    {
+                        rezultat[i] = (char)((rezultat[i] - 'A' + cheie + 26) % 26 + 'A');
+                    }
+                }
+
+                return new string(rezultat);
+            }
+
+            static void Criptanaliza(string text)
+            {
+                Console.WriteLine("\nCriptanaliza:");
+                for (int k = 1; k <= 25; k++)
+                {
+                    Console.WriteLine($"Cheia {k}: {Cezar(text, -k)}");
+                }
+            }
+
+            static void Main()
+            {
+                Console.Write("Introdu textul: ");
+                string text = Console.ReadLine();
+
+                Console.WriteLine("\nCriptare (+3): " + Cezar(text, 3));
+                Console.WriteLine("Decriptare (-3): " + Cezar(text, -3));
+
+                Criptanaliza(text);
+            }
+        }
+    }
